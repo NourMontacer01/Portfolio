36a92dea05a042d0f1242f14f60613ada204e472
Nour Montacer <Nourmontacer10@gmail.com>
2024-08-03 18:52:36 +0100

    update

--- app/layout.js
+++ app/layout.js
@@ -18,7 +18,7 @@ export default function RootLayout({ children }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" href={personalData.logo}/> {/* Use for favicon */}
+        <link rel="icon" href={personalData.profile}/> {/* Use for favicon */}
        
         <title>Nour Montacer&apos;s Portfolio</title>
       </head>
