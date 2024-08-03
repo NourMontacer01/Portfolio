61925335de2356b933d6be840cf72c5b44383295
Nour Montacer <Nourmontacer10@gmail.com>
2024-08-03 20:07:17 +0100

    correct experience data

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -4,12 +4,12 @@ export const experiences = [
     id: 1,
     title: "2-months internship",
     company: "LTS - leader solution tactile",
-    duration: "(Jun 2023 - August 2024)"
+    duration: "(Jun 2024 - August 2024)"
   },
   {
     id: 2,
     title: "2-months internship",
     company: "Case Tunisia ",
-    duration: "(Jun 2022 - August 2023)"
+    duration: "(Jun 2023 - August 2023)"
   }
 ]
